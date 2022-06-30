fb68d05ddf1b0ec7610d25b2e3215c01c7bed517
Eugene Ignashov <eugene.ign@yandex.ru>
2022-06-30 11:19:48 +0500

    добавлен user controller

---
+++ src/main/java/com/example/springsecurity2/controller/AdminController.java
@@ -0,0 +1,63 @@
+package com.example.springsecurity2.controller;
+
+
+import com.example.springsecurity2.model.User;
+import com.example.springsecurity2.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final UserService userService;
+
+    @Autowired
+    public AdminController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public String findAll(Model model){
+        List<User> users = userService.findAll();
+        model.addAttribute("users", users);
+        return "user-list";
+    }
+
+    @GetMapping("/user-create")
+    public String createUserForm(User user){
+        return "user-create";
+    }
+
+    @PostMapping("/user-create")
+    public String createUser(User user){
+        userService.saveUser(user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("user-delete/{id}")
+    public String deleteUser(@PathVariable("id") Long id){
+        userService.deleteById(id);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/user-update/{id}")
+    public String updateUserForm (@PathVariable("id") Long id, Model model){
+        User user = userService.findById(id);
+        model.addAttribute("user", user);
+        return "/user-update";
+    }
+
+    @PostMapping("/user-update")
+    public String updateUser(User user){
+        userService.saveUser(user);
+        return "redirect:/admin";
+    }
+}
--- src/main/java/com/example/springsecurity2/controller/UserController.java
+++ src/main/java/com/example/springsecurity2/controller/UserController.java
@@ -1,18 +1,15 @@
 package com.example.springsecurity2.controller;
 
-
-import com.example.springsecurity2.model.User;
 import com.example.springsecurity2.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
@@ -22,40 +19,9 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/users")
-    public String findAll(Model model){
-        List<User> users = userService.findAll();
-        model.addAttribute("users", users);
-        return "user-list";
-    }
-
-    @GetMapping("/user-create")
-    public String createUserForm(User user){
-        return "user-create";
-    }
-
-    @PostMapping("/user-create")
-    public String createUser(User user){
-        userService.saveUser(user);
-        return "redirect:/users";
-    }
-
-    @GetMapping("user-delete/{id}")
-    public String deleteUser(@PathVariable("id") Long id){
-        userService.deleteById(id);
-        return "redirect:/users";
-    }
-
-    @GetMapping("/user-update/{id}")
-    public String updateUserForm (@PathVariable("id") Long id, Model model){
-        User user = userService.findById(id);
-        model.addAttribute("user", user);
-        return "/user-update";
-    }
-
-    @PostMapping("/user-update")
-    public String updateUser(User user){
-        userService.saveUser(user);
-        return "redirect:/users";
+    @GetMapping()
+    public String getUser(Model model, Authentication authentication) {
+        //скоро тут будет метод
+        return "user";
     }
 }
