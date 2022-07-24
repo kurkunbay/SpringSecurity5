c5bf519c5e101b12fd1a74069320ac1ccbecce51
Eugene Ignashov <eugene.ign@yandex.ru>
2022-07-24 22:20:07 +0500

    поправил запросы

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,35 +1,46 @@
--- Table: users
-CREATE TABLE user (
-  id       INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL
+-- Table: user
+CREATE TABLE user
+(
+    id       INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
 )
-  ENGINE = InnoDB;
+    ENGINE = InnoDB;
 
--- Table: roles
-CREATE TABLE roles (
-  id   INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100) NOT NULL
+-- Table: role
+CREATE TABLE role
+(
+    id   INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
 )
-  ENGINE = InnoDB;
+    ENGINE = InnoDB;
 
 -- Table for mapping user and roles: user_roles
-CREATE TABLE user_roles (
-  user_id INT NOT NULL,
-  role_id INT NOT NULL,
+CREATE TABLE user_roles
+(
+    user_id INT NOT NULL,
+    role_id INT NOT NULL,
 
-  FOREIGN KEY (user_id) REFERENCES user (id),
-  FOREIGN KEY (role_id) REFERENCES roles (id),
+    FOREIGN KEY (user_id) REFERENCES user (id),
+    FOREIGN KEY (role_id) REFERENCES role (id),
 
-  UNIQUE (user_id, role_id)
+    UNIQUE (user_id, role_id)
 )
-  ENGINE = InnoDB;
+    ENGINE = InnoDB;
 
 -- Insert data
 
-INSERT INTO user VALUES (99, '$2a$10$BWTeesqrepW1G6R5ff3AyO0rrDPMhQSPH/h10V8d.yQah6je6SUVW', 'Admin');
+INSERT INTO user
+VALUES (99, 'Admin', '$2a$10$BWTeesqrepW1G6R5ff3AyO0rrDPMhQSPH/h10V8d.yQah6je6SUVW');
+INSERT INTO user
+VALUES (100, 'User', '$2a$10$BWTeesqrepW1G6R5ff3AyO0rrDPMhQSPH/h10V8d.yQah6je6SUVW');
 
-INSERT INTO role VALUES (1, 'ROLE_USER');
-INSERT INTO role VALUES (2, 'ROLE_ADMIN');
+INSERT INTO role
+VALUES (1, 'ROLE_USER');
+INSERT INTO role
+VALUES (2, 'ROLE_ADMIN');
 
-INSERT INTO user_roles VALUES (99, 2);
+INSERT INTO user_roles
+VALUES (99, 2);
+INSERT INTO user_roles
+VALUES (100, 1);
